f980d164277e267fbf627702d629684b81bc3549
jaafar-harabi <hjaafar@daytruck.tn>
2025-11-11 21:19:55 +0100

    feat(gateway): route /api/tasks/** and validate JWT

--- services/api-gateway/src/main/java/com/example/api_gateway/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-@Configuration @EnableWebFluxSecurity
-class SecurityConfig {
-  @Bean SecurityWebFilterChain chain(ServerHttpSecurity http){
-    return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-      .authorizeExchange(ex -> ex.pathMatchers("/auth/**","/actuator/**").permitAll().anyExchange().authenticated())
-      .oauth2ResourceServer(o -> o.jwt()).build();
-  }
-}
